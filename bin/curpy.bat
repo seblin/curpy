926744425924d93cdc3743effcb4eae1c22c948e
Sebastian Linke <Seb_Linke@arcor.de>
2019-08-09 23:02:52 +0200

    Add starter scripts
    Add script files for Windows and non-Windows systems to start Curpy from the command-line.

---
+++ bin/curpy.bat
@@ -0,0 +1,20 @@
+:: Copyright (C) 2019  Sebastian Linke
+
+:: This file is part of Curpy.
+
+:: Curpy is free software: you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by
+:: the Free Software Foundation, either version 3 of the License, or
+:: (at your option) any later version.
+
+:: Curpy is distributed in the hope that it will be useful,
+:: but WITHOUT ANY WARRANTY; without even the implied warranty of
+:: MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+:: GNU General Public License for more details.
+
+:: You should have received a copy of the GNU General Public License
+:: along with Curpy.  If not, see <https://www.gnu.org/licenses/>.
+
+:: This is the starter script for Curpy on Windows.
+
+@py -m curpy %*
